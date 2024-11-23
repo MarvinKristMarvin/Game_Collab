d32b6464ffeed3625f370e11a29e501bbf10e1ad
MarvinKristMarvin <166423247+MarvinKristMarvin@users.noreply.github.com>
2024-11-23 22:01:00 +0100

    Modified component and pages

--- frontend/src/components/FixedButtons/FixedButtons.tsx
+++ frontend/src/components/FixedButtons/FixedButtons.tsx
@@ -1,16 +1,41 @@
 import "./FixedButtons.css";
 import Button from "../Button/Button";
 
-function FixedButtons() {
+interface props {
+  filtering: boolean;
+}
+
+function FixedButtons({ filtering }: props) {
   function searchProfiles() {
     console.log("search profiles function");
   }
-  return (
-    <div className="fixedButtonsComponent">
-      {/* ici les fixedButtons à importer */}
-      <Button text="Search profiles" func={searchProfiles} />
-    </div>
-  );
+  function openFilters() {
+    console.log("open filters menu");
+  }
+  function leftProfile() {
+    console.log("left profile");
+  }
+  function rightProfile() {
+    console.log("right profile");
+  }
+
+  if (filtering === true) {
+    return (
+      <div className="fixedButtonsComponent">
+        {/* ici les fixedButtons à importer */}
+        <Button text="Search profiles" func={searchProfiles} />
+      </div>
+    );
+  } else {
+    return (
+      <div className="fixedButtonsComponent grid">
+        {/* ici les fixedButtons à importer */}
+        <Button text="<" func={leftProfile} />
+        <Button text="Filters" func={openFilters} />
+        <Button text=">" func={rightProfile} />
+      </div>
+    );
+  }
 }
 
 export default FixedButtons;
--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -7,94 +7,104 @@ import CheckableItem from "../components/CheckableItem/CheckableItem";
 import { useState } from "react";
 
 function Search() {
-  return (
-    <div className="searchPage">
-      <FixedButtons />
-      <form action="">
-        {/* NAME AND AGE */}
-        <section className="spacingSection">
-          <Label text="Search for minimum and maximum age" htmlFor="minAge" />
-          <div className="jobList">
+  const [filtering, setFiltering] = useState(false);
+
+  if (filtering === true) {
+    return (
+      <div className="searchPage">
+        <FixedButtons filtering={filtering} />
+        <form action="">
+          {/* NAME AND AGE */}
+          <section className="spacingSection">
+            <Label text="Search for minimum and maximum age" htmlFor="minAge" />
+            <div className="jobList">
+              <InputField
+                placeholder="Minimum age"
+                inputType="text"
+                inputId="minAge"
+                inputName="minAge"
+              />
+              <InputField
+                placeholder="Maximum age"
+                inputType="text"
+                inputId="maxAge"
+                inputName="maxAge"
+              />
+            </div>
+          </section>
+
+          {/* LANGUAGES */}
+          <section className="spacingSection">
+            <Label text="Search people talking" htmlFor="english" />
+            <div className="flagList">
+              <CheckableItem text="" inputId="english" language="english" />
+              <CheckableItem text="" inputId="french" language="french" />
+              <CheckableItem text="" inputId="german" language="german" />
+              <CheckableItem text="" inputId="japanese" language="japanese" />
+              <CheckableItem text="" inputId="russian" language="russian" />
+              <CheckableItem text="" inputId="english" language="english" />
+              <CheckableItem text="" inputId="french" language="french" />
+              <CheckableItem text="" inputId="german" language="german" />
+              <CheckableItem text="" inputId="japanese" language="japanese" />
+              <CheckableItem text="" inputId="russian" language="russian" />
+            </div>
+          </section>
+
+          {/* JOBS */}
+          <section className="spacingSection">
+            <Label text="Search people doing" htmlFor="code" />
+            <div className="jobList">
+              <CheckableItem text="Code" inputId="code" />
+              <CheckableItem text="Art" inputId="art" />
+              <CheckableItem text="Modelling" inputId="modelling" />
+              <CheckableItem text="Animation" inputId="animation" />
+              <CheckableItem text="Music" inputId="music" />
+              <CheckableItem text="Composition" inputId="composition" />
+              <CheckableItem text="Sound" inputId="sound" />
+              <CheckableItem text="Test" inputId="test" />
+              <CheckableItem text="Other" inputId="other" />
+            </div>
+          </section>
+
+          {/* REMUNERATION */}
+          <section className="spacingSection">
+            <Label text="Search people working for" htmlFor="free" />
+            <div className="remunerationList">
+              <CheckableItem text="Fun" inputId="free" />
+              <CheckableItem text="Revenue shares" inputId="shares" />
+              <CheckableItem text="Fixed amount" inputId="gratification" />
+              <CheckableItem text="Salary" inputId="salary" />
+            </div>
+          </section>
+
+          {/* KEYWORDS */}
+          <section className="spacingSection ">
+            <Label text="Search for description keywords" htmlFor="keywords" />
             <InputField
-              placeholder="Minimum age"
-              inputType="text"
-              inputId="minAge"
-              inputName="minAge"
+              placeholder="Keywords separated by space"
+              inputType="textarea"
+              inputId="keywords"
+              inputName="keywords"
             />
-            <InputField
-              placeholder="Maximum age"
-              inputType="text"
-              inputId="maxAge"
-              inputName="maxAge"
-            />
-          </div>
-        </section>
-
-        {/* LANGUAGES */}
-        <section className="spacingSection">
-          <Label text="Search people talking" htmlFor="english" />
-          <div className="flagList">
-            <CheckableItem text="" inputId="english" language="english" />
-            <CheckableItem text="" inputId="french" language="french" />
-            <CheckableItem text="" inputId="german" language="german" />
-            <CheckableItem text="" inputId="japanese" language="japanese" />
-            <CheckableItem text="" inputId="russian" language="russian" />
-            <CheckableItem text="" inputId="english" language="english" />
-            <CheckableItem text="" inputId="french" language="french" />
-            <CheckableItem text="" inputId="german" language="german" />
-            <CheckableItem text="" inputId="japanese" language="japanese" />
-            <CheckableItem text="" inputId="russian" language="russian" />
-          </div>
-        </section>
-
-        {/* JOBS */}
-        <section className="spacingSection">
-          <Label text="Search people doing" htmlFor="code" />
-          <div className="jobList">
-            <CheckableItem text="Code" inputId="code" />
-            <CheckableItem text="Art" inputId="art" />
-            <CheckableItem text="Modelling" inputId="modelling" />
-            <CheckableItem text="Animation" inputId="animation" />
-            <CheckableItem text="Music" inputId="music" />
-            <CheckableItem text="Composition" inputId="composition" />
-            <CheckableItem text="Sound" inputId="sound" />
-            <CheckableItem text="Test" inputId="test" />
-            <CheckableItem text="Other" inputId="other" />
-          </div>
-        </section>
-
-        {/* REMUNERATION */}
-        <section className="spacingSection">
-          <Label text="Search people working for" htmlFor="free" />
-          <div className="remunerationList">
-            <CheckableItem text="Fun" inputId="free" />
-            <CheckableItem text="Revenue shares" inputId="shares" />
-            <CheckableItem text="Fixed amount" inputId="gratification" />
-            <CheckableItem text="Salary" inputId="salary" />
-          </div>
-        </section>
-
-        {/* KEYWORDS */}
-        <section className="spacingSection ">
-          <Label text="Search for description keywords" htmlFor="keywords" />
-          <InputField
-            placeholder="Keywords separated by space"
-            inputType="textarea"
-            inputId="keywords"
-            inputName="keywords"
-          />
-        </section>
-        {/*
-        <Button text="Save" func={save} />
-        <Button text="Log out" func={logOut} color="orangeButton" />
-        <Button
-          text="Delete account"
-          func={deleteAccount}
-          color="orangeButton"
-        />*/}
-      </form>
-    </div>
-  );
+          </section>
+          {/*
+          <Button text="Save" func={save} />
+          <Button text="Log out" func={logOut} color="orangeButton" />
+          <Button
+            text="Delete account"
+            func={deleteAccount}
+            color="orangeButton"
+          />*/}
+        </form>
+      </div>
+    );
+  } else {
+    return (
+      <div className="searchPage">
+        <FixedButtons />
+      </div>
+    );
+  }
 }
 
 export default Search;
