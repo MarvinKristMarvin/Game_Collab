71391efa4e8f25052baca7e6914f6234cc39bc9e
MarvinKristMarvin <166423247+MarvinKristMarvin@users.noreply.github.com>
2025-01-16 03:21:22 +0100

    Created a psql db in render, try to connect to it

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -6,12 +6,30 @@ import path from "path";
 dotenv.config();
 
 // The pool manages multiple connections to the db, it reuses connections to optimize resource usage
-export const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT as string, 10),
+const isProduction = process.env.NODE_ENV === "production";
+export const pool = new Pool(
+  isProduction
+    ? {
+        // Use a connection string and ssl for render
+        connectionString: process.env.DB_URL,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT as string, 10),
+      }
+);
+
+// Test the db connection
+pool.query("SELECT NOW()", (err, res) => {
+  if (err) {
+    console.error("Database connection error:", err);
+  } else {
+    console.log("Database connected at:", res.rows[0].now);
+  }
 });
 
 // Export a function to query the db with optionnal parameters
